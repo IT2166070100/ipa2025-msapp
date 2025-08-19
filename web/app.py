954929af1d5618524c4c6b1b0aab2e2a86cb57a2
IT2166070100 <66070100@kmitl.ac.th>
2025-08-19 18:50:43 +0700

    fix: Compose successfully

--- web/app.py
+++ web/app.py
@@ -1,62 +1,40 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import redirect
-from flask import url_for
+import os
+
+from flask import Flask, request, render_template, redirect
 from pymongo import MongoClient
+from bson import ObjectId
 
-client = MongoClient('mongodb://mongo:27017/')
+app = Flask(__name__)
 
-router_db = client["ipa2025"]
-router_col = router_db["routers"]
+mongo_uri  = os.environ.get("MONGO_URI")
+db_name    = os.environ.get("DB_NAME")
 
-sample = Flask(__name__)
+client = MongoClient(mongo_uri)
+db = client[db_name]
+routers = db["routers"]
 
-data = []
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html", routers=list(routers.find()))
 
-@sample.route("/")
-def main():
-    # Get data from MongoDB and store in data list
-    data.clear()  # Clear existing data
-    mongo_data = router_col.find({}, {"router_ip": 1, "username": 1})
-    for doc in mongo_data:
-        data.append({
-            "router_ip": doc["router_ip"],
-            "username": doc["username"]
-        })
-    return render_template("index.html", data=data)
-
-@sample.route("/add", methods=["POST"])
+@app.route("/add", methods=["POST"])
 def add_router():
-    router_ip = request.form.get("router_ip")
+    ip = request.form.get("ip")
     username = request.form.get("username")
     password = request.form.get("password")
 
-    if router_ip and username and password:
-        # Store in MongoDB
-        router_col.insert_one({
-            "router_ip": router_ip,
+    if ip and username and password:
+        routers.insert_one({
+            "ip": ip,
             "username": username,
             "password": password
         })
-        # Store in data list (only router_ip and username)
-        data.append({"router_ip": router_ip, "username": username})
-    return redirect(url_for("main"))
+    return redirect("/")
 
-@sample.route("/delete/<idx>", methods=["POST"])
-def delete_router(idx):
-    idx = int(idx)
-    try:
-        if 0 <= idx < len(data):
-            # Get the router_ip to delete from MongoDB
-            router_to_delete = data[idx]["router_ip"]
-            # Delete from MongoDB
-            router_col.delete_one({"router_ip": router_to_delete})
-            # Delete from data list
-            data.pop(idx)
-    except Exception:
-        pass
-    return redirect(url_for("main"))
+@app.route("/delete/<id>", methods=["POST"])
+def delete_router(id):
+    routers.delete_one({"_id": ObjectId(id)})
+    return redirect("/")
 
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
