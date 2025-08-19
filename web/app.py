f255126a4c94521fede99d6f241736e40de076c7
IT2166070100 <66070100@kmitl.ac.th>
2025-08-19 14:46:21 +0700

    checkpoint for what've done

--- web/app.py
+++ web/app.py
@@ -3,6 +3,13 @@ from flask import request
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+
+router_db = client["ipa2025"]
+router_col = router_db["routers"]
+
 
 sample = Flask(__name__)
 
@@ -13,16 +20,18 @@ def main():
     return render_template("index.html", data=data)
 
 @sample.route("/add", methods=["POST"])
-def add_comment():
-    yourname = request.form.get("yourname")
-    message = request.form.get("message")
+def add_router():
+    router_ip = request.form.get("router_ip")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-    if yourname and message:
-        data.append({"yourname": yourname, "message": message})
+
+    if router_ip and username and password:
+        data.append({"router_ip": router_ip, "username": username, "password": password})
     return redirect(url_for("main"))
 
 @sample.route("/delete", methods=["POST"])
-def delete_comment():
+def delete_router():
     try:
         idx = int(request.form.get("idx"))
         if 0 <= idx < len(data):
@@ -35,7 +44,7 @@ if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=8080)
 
 @sample.route("/delete/<idx>", methods=["POST"])
-def delete_comment(idx):
+def delete_router(idx):
     idx = int(idx)
     try:
         if 0 <= idx < len(data):
